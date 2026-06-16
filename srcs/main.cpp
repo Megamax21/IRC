27bd5808ed2cfb041256fb1ccb3d12bfc5caca06
Megamax21 <lhotemaxime@hotmail.com>
2026-06-16 18:18:50 +0200

    Base code

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 13:40:45 by ml-hote           #+#    #+#             */
-/*   Updated: 2026/06/16 15:33:02 by ml-hote          ###   ########.fr       */
+/*   Updated: 2026/06/16 18:05:06 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,57 +16,9 @@
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 
-const int PORT = 8080;
-const int BUFFER_SIZE = 1024;
-
-int main() {
-	int server_fd, new_socket;
-	struct sockaddr_in address;
-	int opt = 1;
-	socklen_t addrlen = sizeof(address);
-	char buffer[BUFFER_SIZE] = {0};
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-	// Bind the socket to the network address and port
-	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	// Start listening for incoming connections
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	std::cout << "Server listening on port " << PORT << std::endl;
-	// Accept incoming connection
-	if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	// Read and echo the received message
-	ssize_t valread = read(new_socket, buffer, BUFFER_SIZE);
-	std::cout << "Received: " << buffer << std::endl;
-	send(new_socket, buffer, valread, 0);
-	std::cout << "Echo message sent" << std::endl;
-	// Close the socket
-	close(new_socket);
-	close(server_fd);
+int main(int ac, char **av)
+{
+	
 	return 0;
 }
